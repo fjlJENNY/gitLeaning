cccc8fb105bdb6f8e4ca2bb36c0747a7a71c7b39
fanjianlin <ufo_2014@126.com>
2017-10-09 23:47:28 +0800

    Symbol 添加

--- SymbolExtend/symbol_fun.js
+++ SymbolExtend/symbol_fun.js
@@ -4,3 +4,18 @@ var s2 = Symbol.for();
 var s3 = Symbol.for();
 console.log(s1 === s2);
 console.log(s1 === s3);
+
+// 获取 Symbol.keyFor() 上注册的key
+var s4 = Symbol();
+console.log(Symbol.keyFor(s4));
+var s5 = Symbol("foo");
+console.log(Symbol.keyFor(s5));
+var s6 = Symbol.for("bar");
+console.log(Symbol.keyFor(s6));
+
+// ?? 为甚么会创建2个iframe 
+var iframe = document.createElement("iframe");
+//iframe.src = String(window.location);
+document.body.appendChild(iframe);
+var cur = iframe.contentWindow.Symbol.for("foo") == Symbol.for("foo");
+console.log("p21",cur);
