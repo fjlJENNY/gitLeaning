918ee17208ee0c7302ce622bdbdc8481929a2a5d
fanjl <fanjl@lichengsoft.com>
2017-12-08 18:06:28 +0800

    1.代码添加

--- modules/overview.js
+++ modules/overview.js
@@ -2,6 +2,22 @@
 //console.log(readFile);
 
 
+
+//[a!]
+//CommonJs 模块
+let {start , exists , readFile} = require("fs");
+
+
+// 相当于 整体加载 fs 模块, (这种加载模式称为运行时加载)
+let _fs = require("fs");
+let start = _fs.start;
+
+
+// 而 ES6 中的加载 是从 "编译时加载"  (但是无法引入 ES6 模块的 本身，ES6 不是对象)
+import {start , exists , readFile } from "fs";
+
+
+
 // "ES6" 的模块默认采用严格模式
 //1.evel 和 arguments 不能重新赋值
 //2.arguments 不会自动反映函数参数的变化
