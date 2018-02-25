8c71fe9d9a9ab322adec9c535dfa5809816b3479
fanjl <fanjl@lichengsoft.com>
2018-02-25 13:36:35 +0800

    1.提交

--- financeManagerSys/js/finance/index.js
+++ financeManagerSys/js/finance/index.js
@@ -1,4 +1,35 @@
 var uuid = UUID.generate();
 
 $("#finance_wrapper").find(".user .ui.dropdown").dropdown({});
-$("#finance_wrapper").find(".finance_menu .ui.accordion").accordion({});
+// $("#finance_wrapper").find(".finance_menu .ui.accordion").accordion({});
+
+
+$("#finance_wrapper").find(".finance_menu").click(function(){
+	console.log($(this).find(".item>:target"));
+});
+
+
+$("#finance_wrapper").find(".finance_menu .item .title").click(function(event){
+	event = event||window.event;
+	if(!$(this).hasClass("open")){
+		$(this).parent().parent().find(".open").removeClass("open").next(".content").slideUp(0);
+		$(this).addClass("open");
+		$(this).next(".content").slideDown(200);
+	}else{
+		$(this).addClass("open");
+		$(this).next(".content").slideDown(200);
+	}
+	event.preventDefault();
+	event.stopPropagation();
+	return false;
+});
+
+$("#finance_wrapper").find(".finance_menu .item .title").eq(0).click();
+
+$("#finance_wrapper").find(".finance_menu .item .content").click(function(event){
+	event = event||window.event;
+	var target = event.target;
+	console.log(target);
+	event.stopPropagation();
+	return false;
+});
